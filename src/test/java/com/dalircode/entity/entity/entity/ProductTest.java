cef3a5dbafb9c1fee752462a09297bc1768af0fc
Kamran Dalir <k.dalir@gmail.com>
2020-05-05 16:40:01 +0200

    Initial Commit

---
+++ src/test/java/com/dalircode/entity/entity/entity/ProductTest.java
@@ -0,0 +1,12 @@
+package com.dalircode.entity.entity.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
