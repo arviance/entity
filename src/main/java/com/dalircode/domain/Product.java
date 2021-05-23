797a00dac2b0824639007d4343a113b8c203491b
Kamran Dalir <k.dalir@gmail.com>
2021-05-23 15:51:22 +0200

    added getter and setter in product class

--- src/main/java/com/dalircode/domain/Product.java
+++ src/main/java/com/dalircode/domain/Product.java
@@ -3,4 +3,23 @@ package com.dalircode.domain;
 public class Product {
 
     private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
